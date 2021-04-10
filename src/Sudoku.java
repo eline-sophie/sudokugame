9e92c6c6874863f8a40d4c3809ae2c83a8fdff30
Somentus <somentus@gmail.com>
2021-04-10 17:35:05 +0200

    add javadoc

--- src/Sudoku.java
+++ src/Sudoku.java
@@ -1,13 +1,32 @@
+/**
+ * Represents a sudoku puzzle
+ */
 public class Sudoku {
-    public int[][] puzzle = new int[9][9];
+    /**
+     * Used to store the entries of the sudoku puzzle
+     */
+    public int[][] puzzle;
 
+    /**
+     * Creates an empty sudoku puzzle (9x9 matrix)
+     */
     public Sudoku() {
+        puzzle = new int[9][9];
     }
 
+    /**
+     * Fills a specified value (1-9) in at specified x- and y-coordinates in the sudoku puzzle
+     * @param x     x-coordinate of the square where the value is filled in
+     * @param y     y-coordinate of the square where the value is filled in
+     * @param value value to be filled in
+     */
     public void fill(int x, int y, int value) {
         puzzle[y][x] = value;
     }
 
+    /**
+     * Prints the sudoku puzzle with lines drawn to separate the 3x3 blocks
+     */
     public void print() {
         int j = 0;
         for(int[] row : puzzle) {
